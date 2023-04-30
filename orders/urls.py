faed3de32ca6b6683ddecd5bc541026bd434773f
Your Name <ilan.graterol@hotmail.com>
2023-04-30 11:52:13 -0500

    Añadida vista para actualizar el estado de la orden (OrderUpdateView)

--- orders/urls.py
+++ orders/urls.py
@@ -1,7 +1,7 @@
 # orders/urls.py
 from django.urls import path, include
 from rest_framework import routers
-from .views import OrderViewSet, OrderStatusView
+from .views import OrderViewSet, OrderStatusView, OrderUpdateView
 
 router = routers.DefaultRouter()
 router.register(r'orders', OrderViewSet)
@@ -9,9 +9,7 @@ router.register(r'orders', OrderViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('orders/<int:pk>/status/', OrderStatusView.as_view(), name='order-status'),
+    path('order-status/', OrderStatusView.as_view(), name='order-status-root'),
+    path('orders/<int:id>/update/', OrderUpdateView.as_view(), name='order_update'),
 ]
 
-urlpatterns += router.urls
-
-# Agregamos la siguiente línea para incluir el endpoint de order-status en la raíz de la API
-urlpatterns.append(path('order-status/', OrderStatusView.as_view(), name='order-status-root'))
--- orders/views.py
+++ orders/views.py
@@ -1,7 +1,12 @@
+
 from django.shortcuts import render
-from rest_framework import viewsets, generics
 from rest_framework.response import Response
+from rest_framework import viewsets, generics
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+from django.http import JsonResponse
 from .models import Order
+from django.views import View
 from .serializers import OrderSerializer
 
 class OrderViewSet(viewsets.ModelViewSet):
@@ -17,4 +22,15 @@ class OrderStatusView(generics.RetrieveAPIView):
         order = self.get_object()
         processed_status = order.processed_status
         return Response({'processed_status': processed_status})
+    
+class OrderUpdateView(generics.UpdateAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    lookup_field = 'id' # este es el nombre del campo por el cual buscarás la orden
 
+    def partial_update(self, request, *args, **kwargs):
+        order = self.get_object()
+        serializer = self.get_serializer(order, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
